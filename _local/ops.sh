a9bb3070e39f30533e0e63139343608450e9d28a
mirage335 <spamfreemirage335@gmail.com>
2020-05-28 20:10:10 -0400

    Draft.

--- _local/ops.sh
+++ _local/ops.sh
@@ -1,17 +1,25 @@
+
+
+# WARNING: Board type may not be correctly set in arduinoide .
+# WARNING: May not be appropriate for all relevant projects. Intended as sane defaults ONLY.
+_declare_arduino_device_mega2560
+_declare_arduino_installation_1.8.5
+
+
 #Enable search if "vm.img" and related files are missing.
 export ubVirtImageLocal="false"
 
-# _arduino_example_blink() {
-# 	_arduino_run "$scriptLib"/Blink
-# }
-# 
-# 
-# _task_scope_arduinoide_blink() {
-# 	_scope_arduinoide "$scriptLib"/Blink "$@"
-# }
-# 
-# 
-# _refresh_anchors_task() {
-# 	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_task_scope_arduinoide_blink
-# }
+# # ATTENTION: Add to ops!
+_refresh_anchors_arduino_rewrite() {
+	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_arduino_rewrite
+}
+
+
+_rewrite_arduinoide() {
+	find . -maxdepth 1 -type f -name "*.h" -exec sed -i 's/utility\/u8g.h/clib\/u8g.h/g' {} \;
+	find . -maxdepth 1 -type f -name "*.cpp" -exec sed -i 's/utility\/u8g.h/clib\/u8g.h/g' {} \;
+
+	find . -maxdepth 1 -type f -name "*SdBaseFile.h" -exec sed -i 's/fpos_t/fpos_t1/g' {} \;
+	find . -maxdepth 1 -type f -name "*SdBaseFile.cpp" -exec sed -i 's/fpos_t/fpos_t1/g' {} \;
+}
 
